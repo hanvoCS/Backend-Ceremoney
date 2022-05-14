1ea400f95ab70d2ef7282dea1bf8f278ea6c698c
hanvoCS <alhanoof.maz@gmail.com>
2022-05-14 05:07:31 +0300

    second commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const app = require('./app'); 
+
+
+
+
+
+let host = process.env.PORT; 
+if(host == null || host == ""){
+     host = 4000;
+}
+//Server 
+app.listen(host, ()=>{
+    console.log("Server Running");
+}); 
